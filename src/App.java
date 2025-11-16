ad5a230484fdefd6b52c205d4690cca6508946a3
Gabriel Rocha <biel.mrocha18@gmail.com>
2025-11-16 10:33:07 -0300

    Upando base

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       JOptionPane.showMessageDialog(null, "Bom dia night city", null, 0);
+    }
+}
